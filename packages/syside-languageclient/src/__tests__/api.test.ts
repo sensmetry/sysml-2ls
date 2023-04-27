76669842d2e9454e9c6729af7ba470f37deb5aa7
Daumantas Kavolis <daumantas.kavolis@sensmetry.com>
2023-04-27 06:33:48 +0000

    Chore/client

--- packages/syside-languageclient/src/__tests__/api.test.ts
+++ packages/syside-languageclient/src/__tests__/api.test.ts
@@ -14,6 +14,22 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-export * from "./api";
-export * from "./download";
-export * from "./sysml-language-client";
+import { LanguageClientExtension } from "../api";
+
+describe("Language client extension", () => {
+    const keys = ["onBeforeStart", "onStarted", "onDeactivate"];
+
+    it("should assert true for objects with all keys implemented", () => {
+        expect(
+            LanguageClientExtension.is(Object.fromEntries(keys.map((k) => [k, jest.fn()])))
+        ).toBeTruthy();
+    });
+
+    it.each(keys)("should assert false for objects with '%s' missing", (key) => {
+        expect(
+            LanguageClientExtension.is(
+                Object.fromEntries(keys.filter((k) => k !== key).map((k) => [k, jest.fn()]))
+            )
+        ).toBeFalsy();
+    });
+});
